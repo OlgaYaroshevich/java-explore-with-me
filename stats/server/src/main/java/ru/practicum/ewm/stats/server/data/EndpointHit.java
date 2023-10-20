d8418b5b0290e48ee5cbc4198bb31e51ac317f12
OlgaYaroshevich <Olgagolka.ya@yandex.ru>
2023-10-20 11:37:53 +0300

    stat svc

---
+++ stats/server/src/main/java/ru/practicum/ewm/stats/server/data/EndpointHit.java
@@ -0,0 +1,30 @@
+package ru.practicum.ewm.stats.server.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "endpoints_hits")
+public class EndpointHit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "app")
+    private String app;
+
+    @Column(name = "uri")
+    private String uri;
+
+    @Column(name = "ip")
+    private String ip;
+
+    @Column(name = "timestamp")
+    private LocalDateTime timestamp;
+}
